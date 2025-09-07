1569b9bac0e5868199495d7bb268a9188adbd786
Felicia Melany <feliciamelany01@gmail.com>
2025-09-07 18:05:16 -0300

    Concertando dotenv

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
-import dotevn from "dotevn";
+import dotenv from "dotevn";
 
-dotevn.config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ||3000;
